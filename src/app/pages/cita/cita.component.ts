cbfb5e44ef6ded1836e0044bf2064e6102b4dbcc
Robinson <Robinson@MacBook-Pro-de-Robin.local>
2025-10-20 11:18:53 -0500

    initial commit

---
+++ src/app/pages/cita/cita.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cita',
+  imports: [],
+  templateUrl: './cita.component.html',
+  styleUrl: './cita.component.scss'
+})
+export class CitaComponent {
+
+}
